4d55819e69e953f695f064f74953271cd8b0ee15
Harishanth40 <hari4631119f@gmail.com>
2026-07-28 22:14:31 +0530

    Time: 1 ms (99.90%) | Memory: 46.8 MB (65.58%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> ans= new ArrayList<>();
         for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
+            if(i%15==0){
                 ans.add("FizzBuzz");
             }
             else if(i%3==0){
